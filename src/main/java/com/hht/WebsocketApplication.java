b10697cc99209e34ab255179101cb1f5946fc443
zhangguokang <1030403684@qq.com>
2018-12-29 10:06:54 +0800

    111

--- src/main/java/com/hht/WebsocketApplication.java
+++ src/main/java/com/hht/WebsocketApplication.java
@@ -17,6 +17,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
  * @description
  */
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
+
 public class WebsocketApplication {
     /**
      * @param args
